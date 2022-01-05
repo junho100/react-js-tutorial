98581db1ae6730f902972980cf553ba1cafcd21b
JunHoBaek <bemodesty306@naver.com>
2022-01-05 23:18:21 +0900

    todo list 완료

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,5 +1,39 @@
+import { React, useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => {
+    return setToDo(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+
+    setToDos((current) => [toDo, ...current]);
+    setToDo("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your todo"
+        />
+        <button>Submit</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((toDo, idx) => (
+          <li key={idx}>{toDo}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
