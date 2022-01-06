733b602f51e2270b6760439d20a6dcef610a3384
JunHoBaek <bemodesty306@naver.com>
2022-01-06 23:24:42 +0900

    coin-tracker code challenge 완료

--- coin-tracker/src/App.js
+++ coin-tracker/src/App.js
@@ -5,6 +5,39 @@ import { useState, useEffect } from "react";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [presentCoinPrice, setPresentCoinPrice] = useState(undefined);
+
+  const onChangePresent = (e) => {
+    setPresentCoinPrice(parseFloat(e.target.value));
+    setMoney(0);
+  };
+
+  const onChangeMoney = (e) => {
+    if (e.target.value === "") {
+      return setMoney(0);
+    }
+    setMoney(parseFloat(e.target.value));
+  };
+
+  const renderForm = () => {
+    if (presentCoinPrice === undefined) {
+      return null;
+    }
+
+    return (
+      <div>
+        <input
+          value={money === 0 ? null : money}
+          onChange={onChangeMoney}
+        ></input>
+        <input
+          disabled
+          value={money === 0 ? null : money / presentCoinPrice}
+        ></input>
+      </div>
+    );
+  };
 
   useEffect(() => {
     axios
@@ -13,22 +46,30 @@ function App() {
         return res.data;
       })
       .then((data) => {
-        setCoins(data);
+        setCoins(data.slice(0, 100));
         setIsLoading(false);
       });
   }, []);
   return (
     <div className="App">
       <h1>{isLoading ? "Loading..." : null}</h1>
-      <ul>
-        {coins.map((coin) => {
-          return <li key={coin.id}>{coin.name}</li>;
-        })}
-      </ul>
+      <select onChange={onChangePresent}>
+        {[
+          <option key="default" defaultValue>
+            select coin
+          </option>,
+          coins.map((coin) => {
+            return (
+              <option value={coin.quotes.USD.price} key={coin.id}>
+                {coin.name}
+              </option>
+            );
+          }),
+        ]}
+      </select>
+      <div>{renderForm()}</div>
     </div>
   );
 }
-
-// TODO : select 태그 이용하여 환전 구현해보기
 
 export default App;
